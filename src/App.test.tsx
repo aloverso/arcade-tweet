3ec96d9d5ef4aa28d1417e0db2cc4451d635ce7b
Anne LoVerso <anne.loverso@gmail.com>
2020-02-10 15:45:19 -0700

    Linting fixes - add semicolons

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,10 +4,10 @@ import App from './App';
 
 
 describe('<App />', () => {
-  let subject: RenderResult
+  let subject: RenderResult;
 
   beforeEach(() => {
-    subject = render(<App />)
+    subject = render(<App />);
   });
 
   it('renders A characters', () => {
@@ -16,53 +16,53 @@ describe('<App />', () => {
   });
 
   it('starts with the first character highlighted', () => {
-    const aaa = subject.getAllByText('A')
-    expect(aaa[0]).toHaveClass("active")
-    expect(aaa[1]).not.toHaveClass("active")
+    const aaa = subject.getAllByText('A');
+    expect(aaa[0]).toHaveClass("active");
+    expect(aaa[1]).not.toHaveClass("active");
   });
 
   it('cycles character forward on down button click', () => {
-    const char = subject.getAllByText('A')[0]
-    expect(char.textContent).toEqual('A')
-    fireEvent.click(subject.getByTitle('cycle forward'))
-    expect(char.textContent).toEqual('B')
+    const char = subject.getAllByText('A')[0];
+    expect(char.textContent).toEqual('A');
+    fireEvent.click(subject.getByTitle('cycle forward'));
+    expect(char.textContent).toEqual('B');
   })
 
   it('cycles character backward on up button click', () => {
-    const char = subject.getAllByText('A')[0]
-    fireEvent.click(subject.getByTitle('cycle forward'))
-    expect(char.textContent).toEqual('B')
-    fireEvent.click(subject.getByTitle('cycle backward'))
-    expect(char.textContent).toEqual('A')
+    const char = subject.getAllByText('A')[0];
+    fireEvent.click(subject.getByTitle('cycle forward'));
+    expect(char.textContent).toEqual('B');
+    fireEvent.click(subject.getByTitle('cycle backward'));
+    expect(char.textContent).toEqual('A');
   })
 
   it('wraps character cycling', () => {
-    const char = subject.getAllByText('A')[0]
+    const char = subject.getAllByText('A')[0];
     for (let i = 0; i < 30; i++) {
-      fireEvent.click(subject.getByTitle('cycle forward'))
+      fireEvent.click(subject.getByTitle('cycle forward'));
     }
-    expect(char.textContent).toEqual('_')
-    fireEvent.click(subject.getByTitle('cycle forward'))
-    expect(char.textContent).toEqual('\u00a0')
-    fireEvent.click(subject.getByTitle('cycle backward'))
-    expect(char.textContent).toEqual('_')
-  })
+    expect(char.textContent).toEqual('_');
+    fireEvent.click(subject.getByTitle('cycle forward'));
+    expect(char.textContent).toEqual('\u00a0');
+    fireEvent.click(subject.getByTitle('cycle backward'));
+    expect(char.textContent).toEqual('_');
+  });
 
   it('moves highlight to next character on ok button click', () => {
-    const aaa = subject.getAllByText('A')
-    expect(aaa[0]).toHaveClass("active")
-    expect(aaa[1]).not.toHaveClass("active")
+    const aaa = subject.getAllByText('A');
+    expect(aaa[0]).toHaveClass("active");
+    expect(aaa[1]).not.toHaveClass("active");
 
-    fireEvent.click(subject.getByTitle('next character'))
-    expect(aaa[1]).toHaveClass("active")
-    expect(aaa[0]).not.toHaveClass("active")
-  })
+    fireEvent.click(subject.getByTitle('next character'));
+    expect(aaa[1]).toHaveClass("active");
+    expect(aaa[0]).not.toHaveClass("active");
+  });
 
   it('calls send tweet with the current tweet contents', () => {
-    const tweetLink = subject.getByTitle('tweet it')
-    fireEvent.click(subject.getByTitle('cycle forward'))
-    fireEvent.click(subject.getByTitle('next character'))
-    expect(tweetLink.getAttribute('href')).toContain('B')
+    const tweetLink = subject.getByTitle('tweet it');
+    fireEvent.click(subject.getByTitle('cycle forward'));
+    fireEvent.click(subject.getByTitle('next character'));
+    expect(tweetLink.getAttribute('href')).toContain('B');
     fireEvent.click(subject.getByTitle('next character'));
     expect(tweetLink.getAttribute('href')).toContain('BA');
   });
